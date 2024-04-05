db302d04b5d8d5d505ef77c8eb97b5a337e01edb
Flavio Spedaletti <flavio.spedaletti@adiq.com.br>
2024-04-05 17:07:41 -0300

    Add project files.

---
+++ SoftDeleteEFCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftDeleteEFCore.Models;
+
+namespace SoftDeleteEFCore.Data
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pessoa>(builder =>
+            {
+                
+            });
+        }
+    }
+}
---
+++ SoftDeleteEFCore/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftDeleteEFCore.Models
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+
+        public string Sobrenome { get; set; } = string.Empty;
+    }
+}
